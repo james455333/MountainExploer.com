c1f95ebaa593514014ee57ddab08e9256ec9eb5c
Yurei-Liu <71692471+Yurei-Liu@users.noreply.github.com>
2020-11-02 19:53:24 +0800

    20201102
    MemberBackController

---
+++ src/member/back/controller/MemberBackController.java
@@ -0,0 +1,42 @@
+package member.back.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import member.back.model.MemberBackService;
+import member.model.Member;
+
+@Controller
+public class MemberBackController {
+	
+	@Autowired
+	private MemberBackService memberBackService;
+	
+	@RequestMapping(path = "/memberBack/memberBackEntry.controller", method = RequestMethod.GET)
+	public String processEntryPage() {
+		return "form";
+	}
+	
+	
+	
+	@RequestMapping(path = "/memberBack/memberBackSelectAll.controller", method = RequestMethod.GET)
+	public String processSelectAction1(@RequestParam(name = "account")String account, Model m) {
+		
+		
+		List<Member> mbList = memberBackService.selectAll();
+		m.addAttribute("mbList", mbList);
+		return "memberLsit";
+		
+	}
+	
+	
+
+}
