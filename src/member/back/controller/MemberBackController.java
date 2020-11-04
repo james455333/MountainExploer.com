0e94c27b755af377537604e79e18b76afcdb58e1
Yurei-Liu <71692471+Yurei-Liu@users.noreply.github.com>
2020-11-04 15:13:37 +0800

    update

--- src/member/back/controller/MemberBackController.java
+++ src/member/back/controller/MemberBackController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import member.back.model.MemberBackService;
-import member.model.Member;
 
 @Controller
 public class MemberBackController {
