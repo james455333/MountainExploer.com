cdfc485421ad988b3312b703431e70be13e51dcb
Yurei-Liu <71692471+Yurei-Liu@users.noreply.github.com>
2020-11-14 16:54:58 +0800

    20201114
    register.jsp, js, ajax

--- WebContent/WEB-INF/pages/member/register.jsp
+++ WebContent/WEB-INF/pages/member/register.jsp
@@ -6,22 +6,49 @@
 <head>
 <meta charset="UTF-8">
 <title>會員註冊</title>
-<script src="//code.jquery.com/jquery-3.5.1.js"></script>
+
 </head>
 <body>
     <div>
-        <form action="/member/memberRegister" method="POST">
+        <form action="<c:url value='/member/memberRegister'/>" method="POST">
             <fieldset>
                 <legend>請輸入以下資訊：</legend>
                 <div>
                     <label>帳號：</label>
-                    <input type="text" id="account" class="account" placeholder="請輸入帳號" autocomplete="off"/>
+                    <input type="text" id="account" class="account" name="account" placeholder="請輸入帳號" autocomplete="off"/>
                     <span id="Antsp" class="Antsp"></span><br/>
+                    <input type="button" id="chkAnt" value="檢查帳號">
+                    <span id="chksp"></span><br/>
+                </div>
+                <div>
+                    <label>密碼：</label>
+                    <input type="password" class="pwd" name="pwd" placeholder="請輸入密碼">
+                    <span class="pwdsp"></span><br/>
+                </div>
+                <div>
+                    <label>姓名：</label>
+                    <input type="text" class="name" name="name" placeholder="請輸入姓名" autocomplete="off">
+                    <span class="nmsp"></span><br/>
+                </div>
+                <div>
+                    <label>Email：</label>
+                    <input type="email" class="email" name="email" placeholder="請輸入電子郵件">
+                    <span class="emsp"></span><br/>
+                </div>
+                <div>
+                    <label>身分組選擇</label>
+                    <input type="radio" name="statusId" value="110">一般會員
+                    <input type="radio" name="statusId" value="130">登山嚮導
+                </div>
+                <div>
+                    <input type="button" class="rgtime" value="註冊時間">
+                    <p id="tmsp"></p>
                 </div>
             </fieldset>
         </form>
     </div>
     
-<script src="register.js"></script>
+    <script src="//code.jquery.com/jquery-3.5.1.js"></script>
+	<script src="register.js"></script>
 </body>
 </html>
--- WebContent/member/register.js
+++ WebContent/member/register.js
@@ -1,4 +1,32 @@
-$("#account").on("blur", function(){
+$(".account").on("blur", function(){
+    let AntVal = $(".account").val();
+    let AntValLen = AntVal.length;
+    let flag = false;
+
+    if(AntVal == ""){
+        $(".Antsp").html("<font color='red'>請輸入帳號</font>");
+    }else if(AntValLen >= 6){
+        let re = new RegExp(/[A-Za-z]+[0-9]/);
+        let re2 = new RegExp(/[0-9]+[A-Za-z]/);
+        if(AntVal.match(re)){
+            flag = true;
+        }else if(AntVal.match(re2)){
+            flag = true;
+        }else{
+            flag = false;
+        }
+        if(flag){
+            $(".Antsp").html("<font color='green'>正確</font>");
+        }else{
+            $(".Antsp").html("<font color='red'>帳號長度必須包含英文字母和數字</font>");
+        }
+    }else{
+        $(".Antsp").html("<font color='red'>帳號長度至少6個字元</font>");
+    }
+});
+
+
+$("#chkAnt").on("click", function(){
     var userAnt = $.trim($("#account").val());
 
     $.ajax({
@@ -9,12 +37,97 @@ $("#account").on("blur", function(){
         complete:function(msg){
             console.log(msg);
             if(eval("(" + msg.responseText + ")")){
-                $("#Antsp").html("<font color='red'>帳號已經存在</font>")
+                $("#chksp").html("<font color='red'>帳號已經存在</font>")
             } else{
-                $("#Antsp").html("<font color='blue'>帳號可以使用</font>");
+                $("#chksp").html("<font color='green'>帳號可以使用</font>");
+            }
+        }
+    })
+});
+
+
+$(".pwd").on("blur", function(){
+    let PwdVal = $(".pwd").val();
+    let PwdValLen = PwdVal.length;
+    let flag = false;
+
+    if(PwdVal == ""){
+        $(".pwdsp").html("<font color='red'>請輸入密碼</font>");
+    }else if(PwdValLen >= 8){
+        let re = new RegExp(/[A-Za-z]+[0-9]/);
+        let re2 = new RegExp(/[0-9]+[A-Za-z]/);
+        if(PwdVal.match(re)){
+            flag = true;
+        }else if(PwdVal.match(re2)){
+            flag = true;
+        }else{
+            flag = false;
+        }
+        
+        if(flag){
+            $(".pwdsp").html("<font color='green'>正確</font>");
+        }else{
+            $(".pwdsp").html("<font color='red'>密碼必須包含英文字母與數字</font>");
+        }
+    }else{
+        $(".pwdsp").html("<font color='red'>密碼長度至少8個字元</font>");
+    }
+});
+
+
+$(".name").on("blur", function(){
+    let NmVal = $(".name").val();
+    let NmValLen = NmVal.length;
+    let flag = false;
+
+    if(NmVal == ""){
+        $(".nmsp").html("<font color='red'>請輸入姓名</font>");
+    }else if(NmValLen >= 2){
+        for(let i = 0; i < NmValLen; i++){
+            let ch = NmVal.charCodeAt(i);
+            if(ch >= 0x4e00 && ch <= 0x9fff){
+                flag = true;
+            }else{
+                flag = false;
+                break;
             }
         }
 
+        if(flag){
+            $(".nmsp").html("<font color='green'>正確</font>");
+        }else{
+            $(".nmsp").html("<font color='red'>請輸入中文字元</font>");
+        }
+    }else{
+        $(".nmsp").html("<font color='red'>姓名長度至少2個中文字元</font>");
+    }
+})
 
-    });
-});
+
+$(".email").on("blur", function(){
+    let EmVal = $(".email").val();
+    let EmValLen = EmVal.length;
+    let flag = false;
+
+    if(EmVal == ""){
+        $(".emsp").html("<font color='red'>請輸入電子郵件</font>");
+    }else if(EmValLen >= 5){
+        let re = new RegExp(/^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/);
+        if(EmVal.match(re)){
+            flag = true;
+        }else{
+            flag = false;
+        }
+
+        if(flag){
+            $(".emsp").html("<font color='green'>正確</font>");
+        }else{
+            $(".emsp").html("<font color='red'>電子郵件格式不正確</font>")
+        }
+    }
+})
+
+
+$(".rgtime").on("click", function(){
+    
+})
