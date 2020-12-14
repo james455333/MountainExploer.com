094745f1c000554823dbce71f5eb1673c85ad943
sshong <redgod3630@gmail.com>
2020-12-14 14:52:46 +0800

    1

--- src/ecpay/payment/integration/AllInOne.java
+++ src/ecpay/payment/integration/AllInOne.java
@@ -10,9 +10,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
+
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.helpers.Loader;
+
 import ecpay.payment.integration.domain.ATMRequestObj;
 import ecpay.payment.integration.domain.AioCheckOutALL;
 import ecpay.payment.integration.domain.AioCheckOutATM;
