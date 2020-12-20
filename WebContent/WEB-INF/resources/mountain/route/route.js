0ca9a4afd6bc91fbd88914c8bd2685a6c32481aa
james455333 <71597995+james455333@users.noreply.github.com>
2020-12-20 08:47:41 +0800

    routelist complete ; start back load speed debug

--- WebContent/WEB-INF/resources/mountain/route/route.js
+++ WebContent/WEB-INF/resources/mountain/route/route.js
@@ -1,4 +1,6 @@
 
+
+
 $(function(){
 	PBBlock({
 		countTimes : 3,
@@ -6,18 +8,26 @@ $(function(){
 	})
 	openBlock()
 	progressCount("頁面元素載入完成")
-	$(".npList").on("ready",ajaxDefault())
-	.on("click","div",function(){
-		let npID = $(this).find("button").val()
-		ajaxTN(npID);
-	})
+	ajaxDefault()
 	$(".npList").on("click",'a',function(){
-		$(this).tab('show');
-		ajaxTN( $(this).attr("href").replace("#np",""))
+		hideBook()
+		.then( success =>{
+			console.log("make rt-book display none")
+			setTimeout(()=>{
+				$('#rt-book').remove()
+				new Promise((resolve)=>{
+					console.log("go get data")
+					ajaxTN( $(this).attr("href").replace("np",""))
+					resolve("success")
+				})
+			},1000)
+		})
+		
+
+
 	})
 	$("#rt-info-container").on("click",".btn",function(){
 		let rtID = $(this).val()
-		console.log(rtID)
 		ajaxVTN(rtID, $(this));
 	})
 	$(".page-link").on("click",function(e){
@@ -25,5 +35,9 @@ $(function(){
 		$( '#rt-book' ).bookblock(order);
 		e.preventDefault()
 	})
-	
+	$("#rtSelect").on("change",function(){
+		console.log( $(this)[0].selectedIndex )
+		$("#rt-book").bookblock("jump",$(this)[0].selectedIndex+1)
+	})
 })
+
